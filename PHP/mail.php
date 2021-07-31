f74ff2b208a5d5051d8edcfaa53224817b9fd411
mnhtechidea <88222752+mnhtechidea@users.noreply.github.com>
2021-07-31 11:17:42 +0530

    Update mail.php

--- PHP/mail.php
+++ PHP/mail.php
@@ -3,10 +3,11 @@
         $name=$_POST['Fname'];
         $email=$_POST['email'];
         $comment=$_POST['comment'];
-        $to="mrmuhannadh@gmail.com";
+        $to="mnhtechidea@gmail.com";
         $subject="Comment from Website";
         $headers="From: ".$email; 
         $message="Name: ".$name."\nEmail: ".$email."\nMessage: ".$comment;
+        mail($to,$subject,$message);
         if(mail($to,$subject,$message,$headers)){
             echo "<h1>Sent</h1>";
         }else{
